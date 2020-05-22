365fefc548f3fd97ee879224eef3593df3f80ef5
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-05-21 21:03:26 -0300

    script de Contacto

---
+++ contacto.php
@@ -0,0 +1,19 @@
+<?php
+// captura de variables
+$nombre=$_POST['nombre'];
+$email=$_POST['email'];
+$consulta=$_POST['consulta'];
+
+// cuerpo del mail
+$mensaje="
+<strong>Nombre:</strong> $nombre<br/>
+<strong>email:</strong> $email<br/>
+<strong>consulta:</strong> $consulta<br/>
+";
+
+// envío del mail
+mail("micorreo@midominio.com", "Consulta Web", $mensaje, "From: $nombre <$email>\nReply-To:$email\nContent-Type: text/html; charset=utf-8\n");
+
+//Redireccionamiento a pagina Web
+header ("Location: http://www.misitio.com/mipagina.html");
+?>
