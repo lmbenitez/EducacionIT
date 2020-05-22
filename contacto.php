1bc698b3593b938c3040ffee1414da712a505b23
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-05-21 22:09:15 -0300

    Validacion Formularios

--- contacto.php
+++ contacto.php
@@ -1,18 +1,22 @@
 <?php
 // captura de variables
-$nombre=$_POST['nombre'];
-$email=$_POST['email'];
-$consulta=$_POST['consulta'];
+//VARIABLE: espacio que se reserva en la memoria para almacenar un determinado tipo de dato.
+//PHP 		//HTML
+$nombre 	= $_POST['nombre']; //Cristian
+$email 		= $_POST['email']; //Cristiandracedo@hotmail.com
+$consulta 	= $_POST['consulta']; //Buenas Noches, America!!
+$lenguaje	= $_POST['lenguaje'];
 
 // cuerpo del mail
 $mensaje="
-<strong>Nombre:</strong> $nombre<br/>
-<strong>email:</strong> $email<br/>
-<strong>consulta:</strong> $consulta<br/>
+<strong>Nombre:</strong> $nombre <br/>
+<strong>email:</strong> $email <br/>
+<strong>consulta:</strong> $consulta <br/>
 ";
 
 // envío del mail
-mail("micorreo@midominio.com", "Consulta Web", $mensaje, "From: $nombre <$email>\nReply-To:$email\nContent-Type: text/html; charset=utf-8\n");
+	//DESTINATARIO		//ASUNTO		//CUERPO 	//EMISOR
+mail("info@misitio.com", "Consulta Web", $mensaje, "From: $nombre <$email>\nReply-To:$email\nContent-Type: text/html; charset=utf-8");
 
 //Redireccionamiento a pagina Web
 header ("Location: http://www.misitio.com/mipagina.html");
