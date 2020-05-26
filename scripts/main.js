902aa032698f7b4130910ed597685864d13ba0d2
Cristian Racedo <CristianDRacedo@Hotmail.com>
2020-05-26 02:37:32 -0300

    Scripts XML & JS

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+//Automatizacion del Elemento Progress
+	var miProgreso = document.querySelector('#progressBar');
+	var ingresar = document.querySelector('#miValor');
+
+	function ActualizarProgreso(){
+		miProgreso.value = ingresar.value;
+	}
+
+//Manipulacion del Elemento Video
+	var miVideo = document.querySelector('#miVideo');
+	var reproducir = document.querySelector('#reproducir');
+	var alternar = true
+
+	function Reproducir(){
+			if (alternar) {
+				miVideo.play();
+				reproducir.innerHTML = 'Play';
+				reproducir.style.color = '#eee';
+				reproducir.style.backgroundColor = 'darkslateblue';
+				alternar = false;
+			} else {
+				miVideo.pause();
+				reproducir.innerHTML = 'Paused';
+				reproducir.style.color = '#ccc';
+				reproducir.style.backgroundColor = 'slateblue';
+				alternar = true;
+			}
+		}
+
+//Dibujo simple en Canvas
+	var canvas = document.querySelector('#lienzo');
+	var ctx = canvas.getContext('2d');
+	//Rectangulo a X=10 Y=10 con width=50
+		ctx.fillStyle = 'rgb(200, 0, 0)';
+        ctx.fillRect(10, 10, 50, 50);
+    //Rectangulo a X=30 Y=30 con width=50
+        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        ctx.fillRect(30, 30, 50, 50);
