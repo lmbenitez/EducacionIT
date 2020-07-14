aa4a24253c36a07031d37c9aee6d926a57fabca2
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-07-14 14:23:28 -0300

    elementos HTML5

--- scripts/main.js
+++ scripts/main.js
@@ -7,12 +7,24 @@
 		)
 	//Canvas
 		var ctx = document.querySelector('#jsCanvas').getContext('2d');
-		ctx.fillStyle = "tomato";
-		ctx.fillRect(10,10,100,50);
-		ctx.arc(100, 75, 20, 0, 2*Math.PI);
-		ctx.stroke();
-		ctx.moveTo(200, 0);
-		ctx.lineTo(0,200);
-		ctx.lineTo(50,50);
-		ctx.stroke();
+		//Rectangulo Rojo
+			ctx.fillStyle = "#c30";
+			ctx.fillRect(10,10,100,100);
+		//Circulo AzulCeleste
+			ctx.fillStyle = "rgba(0,80,120,.5)"
+			ctx.arc(110, 110, 50, 50, 100);
+			ctx.fill();
+		//Linea Diagonal Verde
+			ctx.beginPath();
+				ctx.moveTo(200, 0);
+				ctx.lineTo(0,200);
+				ctx.strokeStyle = 'darkgreen';
+				ctx.lineWidth = 10;
+				ctx.stroke();
+			ctx.closePath();
+		//Texto sin Relleno
+			ctx.lineWidth = 1;
+			ctx.strokeStyle = 'black';
+			ctx.font = "16px Century gothic";
+			ctx.strokeText("JavaScript", 110, 190);
 /*Autor: Cristian Racedo*/
